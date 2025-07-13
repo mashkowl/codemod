42687551fb168f828d15f2888b36ee145ed8ff3b
Savina <57533882+mashkowl@users.noreply.github.com>
2025-07-13 16:18:11 +0300

    feat: app base code

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,53 @@
+<script>
+export default {
+  name: 'HelloWorld',
+  data() {
+    return {
+      value: 0
+    }
+  },
+  
+  computed: {
+    value() {
+      return `Счётчик нажатий: ${this.value}`
+    }
+  },
+  
+  methods: {
+    counter() {
+      this.value += 1
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="block">
+    <p>{{value}}</p>
+    
+    <button @click="counter">
+      +1
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.block {
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+}
+
+p {
+  font-size: 16px;
+}
+
+button {
+  border: none;
+  background: #747bff;
+  color: #fff;
+  font-size: 16px;
+  border-radius: 8px;
+  padding: 8px 12px;
+}
+</style>
