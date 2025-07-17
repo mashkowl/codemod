9991f6aa1129a89000be02ad94c4c6b4c364aee3
Savina <57533882+mashkowl@users.noreply.github.com>
2025-07-17 12:12:33 +0300

    feat: new component

---
+++ src/components/CreateComponent.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import ProductCard from './ProductCard.vue';
+
+import { reactive } from 'vue';
+
+const product = reactive({
+  image: '\'https://lifehacker.ru/wp-content/uploads/2024/12/umirotvoryayushhaya_kartinka_dlya_samyx_bespokojnyx_pirozhochkov__1732808526.png\',',
+  name: 'Какое-то название',
+  price: 100,
+})
+
+const addToCart = (product) => {
+  //добавление продукта в корзину
+}
+
+</script>
+
+<template>
+  <ProductCard 
+    :product="product" 
+    @add-to-cart="addToCart" 
+  />
+</template>
+
